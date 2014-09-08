0fb34203e69ddf9b558bd4289b192cde8902f907
Carlos Sanz García <carlos.sanz@gmail.com>
2014-09-08 07:10:22 +0200

    Sepparate DOM form events from the rest

--- src/lib/views/FormView.js
+++ src/lib/views/FormView.js
@@ -1,16 +1,16 @@
-var FormView = Backbone.Marionette.ItemView.extend({
+var FormView = Backbone.Marionette.View.extend({
 
     defaultSchema: {
         ui: null,
         event: null,
-        required: false,
+        validate: false,
         type: 'text',
         message: 'invalid field'
     },
 
-    bindSchema: function() {
+    delegateFormEvents: function() {
 
-        if (_.isEmpty(this.schema)) {
+        if (_.isEmpty(this.schema) || !this.$el) {
             return;
         }
 
@@ -21,8 +21,6 @@ var FormView = Backbone.Marionette.ItemView.extend({
         if (_.isUndefined(this.model)) {
             this.model = new Backbone.Marionette.FormModel();
         }
-
-        this.undelegateEvents();
 
         _.each(this.schema, _.bind(function(_item, key) {
 
@@ -35,26 +33,69 @@ var FormView = Backbone.Marionette.ItemView.extend({
 
             this.ui[key] = this.$el.find(item.ui);
             this.delegate(item.event, item.ui, _.bind(this.saveItem, this), item);
-            if (item.required) {
-                this.listenTo(this.model, 'validate:'+key, this.errorItem);
+            if (item.validate) {
+                this.listenTo(this.model, 'invalid:'+key, this.errorItem);
             }
 
         }, this));
+
+        this.listenTo(this.model, 'invalid', this.invalid);
     },
 
     delegate: function(eventName, selector, listener, options) {
-        this.$el.on(eventName + '.delegateEvents' + this.cid, selector, options, listener);
+        var opts = options || {};
+        this.$el.on(eventName + '.formEvents' + this.cid, selector, opts, listener);
+    },
+
+    undelegateFormEvents: function() {
+        if (this.$el) {
+            this.$el.off('.formEvents' + this.cid);
+        }
+        return this;
+    },
+
+    delegateEvents: function(events) {
+        this._delegateDOMEvents(events);
+        // delegate automatically form events
+        this.delegateFormEvents();
+        this.bindEntityEvents(this.model, this.getOption('modelEvents'));
+        this.bindEntityEvents(this.collection, this.getOption('collectionEvents'));
+        return this;
+    },
+
+    undelegateEvents: function() {
+        var args = Array.prototype.slice.call(arguments);
+        Backbone.View.prototype.undelegateEvents.apply(this, args);
+        // undelegate automatically form events
+        this.undelegateFormEvents();
+        this.unbindEntityEvents(this.model, this.getOption('modelEvents'));
+        this.unbindEntityEvents(this.collection, this.getOption('collectionEvents'));
+        return this;
     },
 
     saveItem: function(event) {
-        if (!event.data || _.isEmpty(event.data)) {
+        var options = event.data || null;
+        if (_.isNull(options)) {
             return;
         }
-        this.model.set(event.data.key, this.ui[event.data.key].val(), {validate: event.data.required, type: event.data.type});
+        this.valid();
+        this.ui[options.key].removeClass('error');
+        this.model.set(options.key, this.ui[options.key].val(), options);
     },
 
-    errorItem: function(key, message) {
-        console.log('validating response.....', message);
+    errorItem: function(options) {
+        if (!options || _.isEmpty(options) || !this.ui[options.key]) {
+            return;
+        }
+        this.ui[options.key].addClass('error');
+    },
+
+    invalid: function() {
+        // override this function
+    },
+
+    valid: function() {
+        // override this function
     }
 
 });
